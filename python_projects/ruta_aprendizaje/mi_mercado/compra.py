69b25ea4935d5496dcb1111e044a3a969206a413
Jhoan C <stivelco@gmail.com>
2024-05-15 08:27:27 -0500

    Medicion de tiempos

--- python_projects/ruta_aprendizaje/mi_mercado/compra.py
+++ python_projects/ruta_aprendizaje/mi_mercado/compra.py
@@ -1,9 +1,31 @@
 # Codigo de la caja de una tienda
+import time # Para medir el tiempo y ejecucion de mi codigo
+import datetime # Para manejar la hora y fecha
+
+def fecha_hora_compra():
+    fecha_hora_actual = datetime.now()
+    fecha_hora_formateada = fecha_hora_actual.strftime("%d-%m-%Y %H:%M")
+    return fecha_hora_formateada
+
+def datos_cliente():
+    cliente = {}
+    nombre = input("Ingrese su nombre:")
+    while True:
+        try:
+            identificacion = int(input("Ingrese su numero de identificación"))
+            break # Fin del bucle si todo esta ok
+        except ValueError:
+            print("Ingrese solo números sin puntos ni comas")
+    # Se almacenan el nombre y el número de identificación en el diccionario cliente.
+    cliente['nombre'] = nombre
+    cliente['identificacion'] = identificacion
+
+    return cliente
 
 def obtener_producto(productos):
     while True: # Mientras sea verdadero se ejecuta este bloque, y termina si escribimos fin.
         producto = input("Ingrese un producto (o escriba 'fin' para terminar): \n").lower()
-        if producto == "fin": # TErmina el algoritmo
+        if producto == "fin": # Termina el algoritmo
             return None
         if producto in productos: # Busca el producto en el diccionario productos
             return producto
@@ -34,28 +56,27 @@ def comprando(productos, impuestos): # Atributos necesarios para el calculo
     precio_total_con_impuesto = 0 # Iniciamos la cuenta en 0
 
     while True:
-        producto = obtener_producto(productos) # Pedir explicacion
-        if producto is None:
+        producto = obtener_producto(productos) # Permite seleccionar productos de la lista
+        if producto is None: # Si no se selecciona productos se cancela
             if not compra:
                 return
             break
 
-        precio = productos[producto]
+        precio = productos[producto] # Se obtiene el precio del producto en el diccionario
         print(f"Producto: {producto.capitalize()}, Precio: ${precio} \n") # Imprime los detalles del producto
 
-        cantidad = obtener_cantidad()
+        cantidad = obtener_cantidad() # Llamamos a la funcion obtener cantidad
 
-        impuesto = impuestos.get(producto, 0)
-        precio_total_producto, precio_total_con_impuesto_producto = calcular_precio_con_impuesto(precio, impuesto, cantidad)
-        #Explicar
+        impuesto = impuestos.get(producto, 0) # Se obtiene el impuesto asociado en el diccionario.
+        precio_total_producto, precio_total_con_impuesto_producto = calcular_precio_con_impuesto(precio, impuesto, cantidad) # Llamamos la funcion calcular precio con impuesto
         precio_total += precio_total_producto
         precio_total_con_impuesto += precio_total_con_impuesto_producto
-        #Explicar
-        if producto in compra:
+
+        if producto in compra: # Si el producto ya está en el registro de compra, se actualiza la cantidad y los precios totales.
             compra[producto]['cantidad'] += cantidad
             compra[producto]['precio_total'] += precio_total_producto
             compra[producto]['precio_total_con_impuesto'] += precio_total_con_impuesto_producto
-        else:
+        else: # Si el producto no está en el registro de compra, se agrega con su cantidad y precios totales.
             compra[producto] = {
                 'cantidad': cantidad,
                 'precio_total': precio_total_producto,
@@ -63,10 +84,10 @@ def comprando(productos, impuestos): # Atributos necesarios para el calculo
             }
 
         agregar = input("¿Deseas agregar otro producto 1.Si / 2.No: ")
-        if agregar != "1":
+        if agregar != "1":  # Si el usuario no ingresa "1", el bucle se rompe.
             break
         print("¡Producto agregado con éxito \n")
-    #Explicar
+    # Muestra los datos almacenados en mi compra
     mostrar_compra(compra)
 
     print(f"\nPrecio Total sin Impuestos: ${precio_total:.2f}")
@@ -86,5 +107,21 @@ impuestos = {
     "salchichas": 0.2  # Impuesto del 20%
 }
 
-#Explicar creo que es porque esta iniciando la funcion
+# Inicializamos las funciones
 comprando(productos, impuestos)
+
+# Tiempos de ejecución
+
+# Funcion Cliente
+
+start_time = time.time() # Captura tiempo antes de la ejecucion
+cliente = datos_cliente()
+end_time = time.time() # Captura tiempo despues de la ejecucion
+print(f"Tiempo de ejecucucion de la funcion datos_cliente: {end_time-start_time:.2f} segundos")
+
+# Funcion comprando
+
+start_time = time.time()
+compra, precio_total, precio_total_con_impuesto, fecha_hora_compra = comprando(productos, impuestos)
+end_time = time.time()
+print(f"Tiempo de ejecución de comprando: {end_time - start_time:.2f} segundos")
